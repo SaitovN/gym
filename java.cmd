e0d1342dda6ee61e1cfd0c136f497b8d08d294f9
N U R S U L T A N <128532192+SaitovN@users.noreply.github.com>
2023-08-19 02:26:20 +0600

    first commit

---
+++ java.cmd
@@ -0,0 +1,5 @@
+rem mkdir %DATE:~-8,8%
+C:\Program Files\Java\jdk-19\bin\java C:\Users\77075\IdeaProjects\java\out\production\java\academy\devonline\java\basic\section01_setup\section02_setup\MkDir.java %DATE:~-8,8%
+copy current %DATE:~-8,8%
+del %DATE:~-8,8%
+rmdir /s /q %DATE:~-8,8%
